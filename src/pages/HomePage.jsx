b3770e5d46c35254dc5f9384306c346f61170765
Pankaj Pote <pankajpote2003@gmail.com>
2026-05-20 23:24:59 +0530

    Hero Banner update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -72,7 +72,7 @@ const HomePage = () => {
     <div className="w-full">
       <HeroCarousel movies={data.hero} />
       
-      <div className="flex flex-col gap-y-0 -mt-2 md:-mt-4 relative z-20 pb-20">
+      <div className="flex flex-col gap-y-0 -mt-6 md:-mt-16 relative z-20 pb-20">
         {data.rows.map(row => (
           <MovieRow key={row.id} title={row.title} movies={row.movies} />
         ))}
